36c1eafe8afac768d2ccc38d12b18c6fd08aaab5
ksnghyk <hani103201@naver.com>
2023-02-13 22:10:25 +0900

    taxi UI 그리고 cursor 조금

--- js/character.js
+++ js/character.js
@@ -1,7 +1,16 @@
-
+const character = document.getElementById('character');
 
 window.addEventListener('keydown', (e) => {
     if(e.key === 'Control'){
-        console.log('hi');
+        character.src = 'img/characterSkill.gif';
+        setTimeout(() => character.src = 'img/character.png' , 1860);
+        function damage (){
+            return Math.floor(Math.random()*200) + 200;
+        }    
+        const damage1 = damage();
+        const damage2 = damage();
+        console.log(damage1, damage2);
     }
 })
+
+
--- js/taxi.js
+++ js/taxi.js
@@ -1,35 +1,50 @@
 const taxi = document.getElementById('taxi');
 const bgm = document.getElementById('bgm');
-const bgAndBgm = document.getElementById('bgAndBgm');
 const background = document.getElementById('background');
+const taxiInfo = document.getElementById('taxiInfo');
 
 function onTaxi(){
-    const taxiText = document.createElement('img');
-    taxiText.src = 'img/임시메모장.png';
-    taxiText.id = 'taxiText';
-    bgAndBgm.appendChild(taxiText);
+    taxiInfo.style.backgroundImage = "url('img/SysOpt.backgrnd.png')";
+    selectTown('헤네시스');
+    selectTown('페리온');
     selectTown('커닝시티');
     selectTown('엘리니아');
     selectTown('슬리피우드');
 }
 
 function selectTown(townName){
-    const town = document.createElement('button');
-    town.innerText = townName;
+    const town = document.createElement('img');
     town.id = townName;
-    bgAndBgm.appendChild(town);
+    town.className = 'townIcon';
+    town.src = `img/${town.id}주문서.png`
+    taxiInfo.append(town);
+    town.addEventListener('mouseenter', cursorSwitch);
+    // town.addEventListener('mouseleave', );
     town.addEventListener('click', moveTown); //스코프 덕분에 이렇게 간결하게 쓸 수 있는건가?
 } 
 
 function moveTown(event){
     background.src = `img/${event.target.id}.png`;
     bgm.src = `music/${event.target.id}.mp3`;
-    bgAndBgm.innerHTML = '';
+    taxiInfo.innerHTML = '';
+    taxiInfo.style.backgroundImage = "url('')";
     bgm.play();
     bgm.addEventListener('ended', function() { 
         this.currentTime = 0;
         this.play();
     }, false); // false는 무엇을 나타내는거지?
+}
+
+
+
+function cursorSwitch(event){
+    document.body.style.cursor = "none";
+    const newCursor = document.createElement('img');
+    newCursor.src = 'img/Cursor.1.gif';
+    newCursor.id = 'newCursor';
+    document.body.appendChild(newCursor);
+    "clientX: ", event.clientX, 
+    "clientY:", event.clientY
 }
 
 taxi.addEventListener('click', onTaxi);
