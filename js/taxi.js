6baca00892b9699345cd8f3cff8e94c972c127e5
ksnghyk <hani103201@naver.com>
2023-02-14 13:36:55 +0900

    cursor 기능구현 완료

--- js/taxi.js
+++ js/taxi.js
@@ -2,6 +2,7 @@ const taxi = document.getElementById('taxi');
 const bgm = document.getElementById('bgm');
 const background = document.getElementById('background');
 const taxiInfo = document.getElementById('taxiInfo');
+let waitingClick = document.querySelectorAll('.waitingClick');
 
 function onTaxi(){
     taxiInfo.style.backgroundImage = "url('img/SysOpt.backgrnd.png')";
@@ -15,12 +16,12 @@ function onTaxi(){
 function selectTown(townName){
     const town = document.createElement('img');
     town.id = townName;
-    town.className = 'townIcon';
+    town.className = 'townIcon waitingClick';
     town.src = `img/${town.id}주문서.png`
     taxiInfo.append(town);
-    town.addEventListener('mouseenter', cursorSwitch);
-    // town.addEventListener('mouseleave', );
     town.addEventListener('click', moveTown); //스코프 덕분에 이렇게 간결하게 쓸 수 있는건가?
+    town.addEventListener('mouseenter', event => event.target.classList.add('clickCursor'));
+    town.addEventListener('mouseleave', event => event.target.classList.remove('clickCursor'));  //맨 아래 코드랑 중복되는 느낌인데 해결할 수 았나?
 } 
 
 function moveTown(event){
@@ -35,16 +36,10 @@ function moveTown(event){
     }, false); // false는 무엇을 나타내는거지?
 }
 
+waitingClick.forEach(element => {
+    element.addEventListener('mouseenter', event => event.target.classList.add('clickCursor'));
+    element.addEventListener('mouseleave', event => event.target.classList.remove('clickCursor'));
+});
 
-
-function cursorSwitch(event){
-    document.body.style.cursor = "none";
-    const newCursor = document.createElement('img');
-    newCursor.src = 'img/Cursor.1.gif';
-    newCursor.id = 'newCursor';
-    document.body.appendChild(newCursor);
-    "clientX: ", event.clientX, 
-    "clientY:", event.clientY
-}
 
 taxi.addEventListener('click', onTaxi);
