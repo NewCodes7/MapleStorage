f033804170030bfdac0f3a305f7f4764c585a4fc
ksnghyk <hani103201@naver.com>
2023-02-21 17:22:25 +0900

    timer 100s 구현

--- js/timer.js
+++ js/timer.js
@@ -2,15 +2,20 @@ const timerNpc = document.getElementById('timerNpc');
 const timer = document.getElementById('TIMER');
 const timerHidden = document.getElementById('timer-hidden');
 const progressBar = document.getElementById('progressBar');
+const progressBar2 = document.getElementById('progressBar2');
 const progressArea = document.getElementById('progress-area');
 const remainingTime = document.getElementById('remainingTime');
+const remainingTime2 = document.getElementById('remainingTime2');
 const timerOption = document.getElementById('timer-option');
 const timerCancelBtn = document.getElementById('timerCancelBtn');
 const timerStartBtn = document.getElementById('timerStartBtn');
 const totalTime = document.getElementById('totalTime');
 const tictacArea = document.getElementById('tictac-area');
+const resultArea = document.getElementById('result-area');
 let ticTac;
+let ticTac2;
 let reset;
+let reset2;
 
 
 function timerStart() {
@@ -25,7 +30,7 @@ function timerStart() {
         const soundEffect = new Audio(`soundEffect/sound${timerSet}.mp3`);
         ticTac = setInterval(counting, 1000);
         reset = setInterval(() => {count=-1; soundEffect.play(); }, timerSet*999); //왜 화살표 함수로?, 1000으로 했을 때 첫 주기에서 1초 더 홀딩함(4초 기다리고 바뀜)
-        setInterval(clear, 2 * 60 * 60 * 1000);
+        setTimeout(clear, 2 * 60 * 60 * 1000); 
         function counting(){
             let remainingTotalSeconds = timerSet - (++count);
             totalCount += 1;
@@ -42,15 +47,50 @@ function timerStart() {
 }) 
 }
 
+function timerStart2(){
+    const timerGetItemNodeList = document.getElementsByName('get-item');
+    timerGetItemNodeList.forEach((node) => {
+        if(node.checked){
+            let remainingTotalSeconds2 = 101;
+            progressBar2.classList.add(`on100s`);
+            const soundEffect2 = new Audio(`soundEffect/sound3.mp3`);
+            ticTac2 = setInterval(counting2, 1000);
+            counting2();
+            reset2 = setInterval(() => {remainingTotalSeconds2=101; soundEffect2.play();}, 100*999);
+            function counting2(){
+                --remainingTotalSeconds2;
+                let remainingMinutes2 = ('0' + Math.floor(remainingTotalSeconds2/60)).slice(-2);
+                let remainingSeconds2 = ('0' + Math.floor(remainingTotalSeconds2%60)).slice(-2);
+                console.log(remainingTotalSeconds2);
+                remainingTime2.innerText = `줍기 남은시간: ${remainingMinutes2}:${remainingSeconds2}`;
+                }
+            }
+        }
+        )
+        timerOption.style.display = 'none';
+        tictacArea.style.display = 'block';
+}
+    
+
 function cancel(){
     clearInterval(ticTac);
+    clearInterval(ticTac2);
     clearInterval(reset);
-    remainingTime.innerText = '';}
+    clearInterval(reset2);
+    progressBar.className = '';
+    remainingTime.innerText = '';
+    progressBar2.className = '';
+    remainingTime2.innerText = '';
+}
+
     
 function clear(){
     const soundFinish = new Audio('soundEffect/sound7200.mp3')
     soundFinish.play();
     cancel();
+    tictacArea.style.display = 'none';
+    resultArea.style.display = 'block';
+
 }
 
 function timerCancel(){
@@ -66,6 +106,7 @@ function popUpTimer(){
     } else{
         timer.style.display = 'block';
         tictacArea.style.display = 'none';
+        resultArea.style.display = 'none';
     }
 }
 
