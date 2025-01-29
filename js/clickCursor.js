a8d6ade7e947645da31e08b24824547d816694bc
NewCodes7 <newcodes7@gmail.com>
2025-01-29 20:12:14 +0900

    feat: timer 팝업 기능 추가

--- js/clickCursor.js
+++ js/clickCursor.js
@@ -1,26 +1,24 @@
-
-
 const waitingClick = document.querySelectorAll('.waitingClick');
 const clickCursor = document.getElementById('clickCursor');
 
-
-
-function addClickCursor(name){
-    name.forEach(element => {
-        element.addEventListener("mousemove", function(event) {
-            const x = event.clientX;
-            const y = event.clientY;
-            clickCursor.style.transform = `translate(${x}px, ${y}px)`;
-          });
-        element.addEventListener("mouseenter", () => {clickCursor.style.display = 'block'; element.style.cursor = 'none';})
-        element.addEventListener("mouseleave", () => {clickCursor.style.display = 'none'})
+function addClickCursor (name) {
+  name.forEach(element => {
+    element.addEventListener('mousemove', function (event) {
+      const x = event.clientX;
+      const y = event.clientY;
+      clickCursor.style.transform = `translate(${x}px, ${y}px)`;
     });
+    element.addEventListener('mouseenter', () => {
+      clickCursor.style.display = 'block';
+      element.style.cursor = 'none';
+    });
+    element.addEventListener('mouseleave', () => {clickCursor.style.display = 'none';});
+  });
 }
 
 addClickCursor(waitingClick);
 
-
 const isMobile = /iPhone|iPad|iPod|Android/i.test(window.navigator.userAgent);
-if(isMobile){
-    clickCursor.remove();
+if (isMobile) {
+  clickCursor.remove();
 }
--- js/timer.js
+++ js/timer.js
@@ -7,7 +7,7 @@ const progressArea = document.getElementById('progress-area');
 const progressArea2 = document.getElementById('progress-area2');
 const remainingTime = document.getElementById('remainingTime');
 const remainingTime2 = document.getElementById('remainingTime2');
-const timerOption = document.getElementById('timer-option');
+const timerOption = document.getElementById('timer-option-area');
 const timerCancelBtn = document.getElementById('timerCancelBtn');
 const timerStartBtn = document.getElementById('timerStartBtn');
 const totalTime = document.getElementById('totalTime');
@@ -32,199 +32,203 @@ let memoryTotalTime;
 let memoryExpcoupon = 0;
 let memoryItem = 0;
 
-const resultInfo = document.getElementsByClassName('resultInfo');
-const soundStart = new Audio('soundEffect/Start.mp3');
-
-
-function timerStart() {
-    soundStart.play();
-
-    clickCursor.style.display = 'none';
-    timerStartBtn.style.cursor = '';
-
-    timerCancelBtn.disabled = false;
-    timerCancelBtn.className = 'ActivationCancel waitingClick3';
-    const waitingClick3 = document.querySelectorAll('.waitingClick3');
-    addClickCursor(waitingClick3);
-
-    timerStartBtn.disabled = true;
-    timerStartBtn.style.backgroundImage = '';
-    timerStartBtn.className = 'DeactivationStart';
-
-
-    clearTimer = setTimeout(clear, 2 * 60 * 60 * 1000);
-    memoryStart = clock.innerText;
-
-//경쿠 카운트
-    let soundEffect;
-    const timerNodeList = document.getElementsByName('timer');
-    timerNodeList.forEach((node) => {
-      if(node.checked)  {
-
-        progressArea.style.display = 'block';
-
-        const timerSet = Number(node.value);
-        let count = -1;
-        
-        counting(); // start 눌렀을 때 바로 타이머 보이도록
-        progressBar.classList.add(`on${timerSet}s`);
-        soundEffect = new Audio(`soundEffect/sound${timerSet}.mp3`);
-        ticTac = setInterval(counting, 1000);
-        reset = setInterval(() => {count=-1; soundEffect.play(); memoryExpcoupon+=1; }, timerSet*999); //왜 화살표 함수로?, 1000으로 했을 때 첫 주기에서 1초 더 홀딩함(4초 기다리고 바뀜)
-        
-        function counting(){
-            let remainingTotalSeconds = timerSet - (++count);
-            
-            let remainingMinutes = ('0' + Math.floor(remainingTotalSeconds/60)).slice(-2);
-            let remainingSeconds = ('0' + Math.floor(remainingTotalSeconds%60)).slice(-2);
-            
-            remainingTime.innerText = `경쿠 남은시간: ${remainingMinutes}:${remainingSeconds}`;
-            
-        }
-        timerOption.style.display = 'none';
-        tictacArea.style.display = 'block';
-    }
-}) 
-if(remainingTime.innerText == ''){
-    progressArea.style.display = 'none';
-}
-
-//총 사냥시간 카운트
-let totalCount = -1;
-resetTotalCount = setInterval(totalCounting, 1000);
-totalCounting();
-function totalCounting(){
-    totalCount += 1;
-    let countUpMinutes = ('0' + Math.floor(totalCount/60)).slice(-2);
-    let countUpSeconds = ('0' + Math.floor(totalCount%60)).slice(-2);
-    totalTime.innerText = `사냥시간: ${countUpMinutes}:${countUpSeconds}`
-}
-
-
-//메소줍기 카운트
-    const timerGetItemNodeList = document.getElementsByName('get-item');
-    timerGetItemNodeList.forEach((node) => {
-        if(node.checked){
-            soundEffect = new Audio('soundEffect/soundOverlap.mp3');
-
-
-            progressArea2.style.display = 'block';
-            let remainingTotalSeconds2 = 101;
-            progressBar2.classList.add(`on100s`);
-            const soundEffect2 = new Audio(`soundEffect/sound100.mp3`);
-            ticTac2 = setInterval(counting2, 1000);
-            counting2();
-            reset2 = setInterval(() => {remainingTotalSeconds2=101; soundEffect2.play(); memoryItem += 1;}, 100*999);
-            function counting2(){
-                --remainingTotalSeconds2;
-                let remainingMinutes2 = ('0' + Math.floor(remainingTotalSeconds2/60)).slice(-2);
-                let remainingSeconds2 = ('0' + Math.floor(remainingTotalSeconds2%60)).slice(-2);
-                remainingTime2.innerText = `줍기 남은시간: ${remainingMinutes2}:${remainingSeconds2}`;
-                }
-            } else {
-                progressArea2.style.display = 'none';
-            }
-        }
-        )
-
-        timerOption.style.display = 'none';
-        tictacArea.style.display = 'block';
-    }
-
-function timerCancel(){
-    tictacArea.style.display = 'none';
-    resultArea.style.display = 'block';
-    cancel();
-    clickCursor.style.display = 'none';
-    timerCancelBtn.style.cursor = '';
-} 
-
-function clear(){
-    const soundFinish = new Audio('soundEffect/sound7200.mp3')
-    soundFinish.play();
-    cancel();
-    tictacArea.style.display = 'none';
-    resultArea.style.display = 'block';
-}
-
-function cancel(){
-    memoryStop = clock.innerText;
-    memoryTotalTime = totalTime.innerText;
-    timerResult();
-    clearInterval(clearTimer);
-    clearInterval(ticTac);
-    clearInterval(ticTac2);
-    clearInterval(reset);
-    clearInterval(reset2);
-    clearInterval(resetTotalCount);
-    progressBar.className = '';
-    remainingTime.innerText = '';
-    progressBar2.className = '';
-    remainingTime2.innerText = '';
-    totalTime.innerText = '';
-
-    timerCancelBtn.disabled = true;
-    timerCancelBtn.style.backgroundImage = '';
-    timerCancelBtn.className = 'DeactivationCancel';
-    
-    timerStartBtn.disabled = false;
-    timerStartBtn.style.backgroundImage = '';
-    timerStartBtn.className = 'ActivationStart waitingClick';
-    timerStartBtn.onclick = timerFirstScreen;
-}
-
-function timerFirstScreen(){
-    for(let i=0; i<resultInfo.length; i++){
-        resultInfo[i].innerText = '';
-    } //  htmlCollection 순회
-    resultArea.style.display = 'none';
-    timerOption.style.display = 'block';
-    timerStartBtn.onclick = timerStart;
-    timer.style.visibility = 'hidden';
-}
-
-function popUpTimer(){
-    if (window.innerWidth < 1128) {
-        deleteTaxiWindow();
-        deleteQuestWindow();
-    }
-    
-
-    if(timer.style.visibility == 'visible'){
-        timer.style.visibility = 'hidden';
-    } else{
-        timer.style.visibility = 'visible';
-        tooltipText.classList.add('hidden')
-    }
-}
-tictacArea.style.display = 'none';
-resultArea.style.display = 'none';
-
-function timerResult(){
-    result_startTime.innerText = `시작: ${memoryStart}` 
-    result_stopTime.innerText = `종료: ${memoryStop}`
-    result_totalCount.innerText = `${memoryTotalTime}`
-    result_expCoupon.innerHTML += `x${memoryExpcoupon}`
-    result_item.innerHTML += `x${memoryItem}`
-}
-
-
-timerNpc.addEventListener('click', popUpTimer);
-timerStartBtn.addEventListener('mouseover', () => timerStartBtn.style.backgroundImage = "url('" + BtOkMouseOver.src + "')");
-timerStartBtn.addEventListener('mouseleave', () => timerStartBtn.style.backgroundImage = "url('" + BtOkNormal.src + "')" );
-timerStartBtn.addEventListener('mousedown', () => timerStartBtn.style.backgroundImage = "url('" + BtOkPressed.src + "')" );
-timerStartBtn.addEventListener('mouseup', () => timerStartBtn.style.backgroundImage = "url('" + BtOkNormal.src + "')" );
-
-timerCancelBtn.addEventListener('mouseover', () => timerCancelBtn.style.backgroundImage = "url('" + BtCancelMouseOver.src + "')");
-timerCancelBtn.addEventListener('mouseleave', () => timerCancelBtn.style.backgroundImage = "url('" + BtCancelNormal.src + "')");
-timerCancelBtn.addEventListener('mousedown', () => timerCancelBtn.style.backgroundImage = "url('" + BtCancelPressed.src + "')");
-timerCancelBtn.addEventListener('mouseup', () => timerCancelBtn.style.backgroundImage = "url('" + BtCancelNormal.src + "')");
-
-
-const button = document.querySelector('.tooltip-button'); //쿼리셀렉터와 클래스네임의 차이점??
+const button = document.querySelector('.tooltip-button');
 const tooltipText = document.querySelector('.tooltip-text');
 
 button.addEventListener('click', () => {
   tooltipText.classList.toggle('hidden');
 });
 
+const resultInfo = document.getElementsByClassName('resultInfo');
+const soundStart = new Audio('soundEffect/Start.mp3');
+
+function timerStart () {
+  soundStart.play();
+
+  clickCursor.style.display = 'none';
+  timerStartBtn.style.cursor = '';
+
+  timerCancelBtn.disabled = false;
+  timerCancelBtn.className = 'ActivationCancel waitingClick3';
+  const waitingClick3 = document.querySelectorAll('.waitingClick3');
+  addClickCursor(waitingClick3);
+
+  timerStartBtn.disabled = true;
+  timerStartBtn.style.backgroundImage = '';
+  timerStartBtn.className = 'DeactivationStart';
+
+  clearTimer = setTimeout(clear, 2 * 60 * 60 * 1000);
+  memoryStart = clock.innerText;
+
+  //경쿠 카운트
+  let soundEffect;
+  const timerNodeList = document.getElementsByName('timer');
+  timerNodeList.forEach((node) => {
+    if (node.checked) {
+
+      progressArea.style.display = 'block';
+
+      const timerSet = Number(node.value);
+      let count = -1;
+
+      counting(); // start 눌렀을 때 바로 타이머 보이도록
+      progressBar.classList.add(`on${timerSet}s`);
+      soundEffect = new Audio(`soundEffect/sound${timerSet}.mp3`);
+      ticTac = setInterval(counting, 1000);
+      reset = setInterval(() => {
+        count = -1;
+        soundEffect.play();
+        memoryExpcoupon += 1;
+      }, timerSet * 999); //왜 화살표 함수로?, 1000으로 했을 때 첫 주기에서 1초 더 홀딩함(4초 기다리고 바뀜)
+
+      function counting () {
+        let remainingTotalSeconds = timerSet - (++count);
+
+        let remainingMinutes = ('0' + Math.floor(remainingTotalSeconds / 60)).slice(-2);
+        let remainingSeconds = ('0' + Math.floor(remainingTotalSeconds % 60)).slice(-2);
+
+        remainingTime.innerText = `경쿠 남은시간: ${remainingMinutes}:${remainingSeconds}`;
+
+      }
+
+      timerOption.style.display = 'none';
+      tictacArea.style.display = 'block';
+    }
+  });
+  if (remainingTime.innerText == '') {
+    progressArea.style.display = 'none';
+  }
+
+  //총 사냥시간 카운트
+  let totalCount = -1;
+  resetTotalCount = setInterval(totalCounting, 1000);
+  totalCounting();
+
+  function totalCounting () {
+    totalCount += 1;
+    let countUpMinutes = ('0' + Math.floor(totalCount / 60)).slice(-2);
+    let countUpSeconds = ('0' + Math.floor(totalCount % 60)).slice(-2);
+    totalTime.innerText = `사냥시간: ${countUpMinutes}:${countUpSeconds}`;
+  }
+
+  //메소줍기 카운트
+  const timerGetItemNodeList = document.getElementsByName('get-item');
+  timerGetItemNodeList.forEach((node) => {
+      if (node.checked) {
+        soundEffect = new Audio('soundEffect/soundOverlap.mp3');
+
+        progressArea2.style.display = 'block';
+        let remainingTotalSeconds2 = 101;
+        progressBar2.classList.add(`on100s`);
+        const soundEffect2 = new Audio(`soundEffect/sound100.mp3`);
+        ticTac2 = setInterval(counting2, 1000);
+        counting2();
+        reset2 = setInterval(() => {
+          remainingTotalSeconds2 = 101;
+          soundEffect2.play();
+          memoryItem += 1;
+        }, 100 * 999);
+
+        function counting2 () {
+          --remainingTotalSeconds2;
+          let remainingMinutes2 = ('0' + Math.floor(remainingTotalSeconds2 / 60)).slice(-2);
+          let remainingSeconds2 = ('0' + Math.floor(remainingTotalSeconds2 % 60)).slice(-2);
+          remainingTime2.innerText = `줍기 남은시간: ${remainingMinutes2}:${remainingSeconds2}`;
+        }
+      } else {
+        progressArea2.style.display = 'none';
+      }
+    },
+  );
+
+  timerOption.style.display = 'none';
+  tictacArea.style.display = 'block';
+}
+
+function timerCancel () {
+  tictacArea.style.display = 'none';
+  resultArea.style.display = 'block';
+  cancel();
+  clickCursor.style.display = 'none';
+  timerCancelBtn.style.cursor = '';
+}
+
+function clear () {
+  const soundFinish = new Audio('soundEffect/sound7200.mp3');
+  soundFinish.play();
+  cancel();
+  tictacArea.style.display = 'none';
+  resultArea.style.display = 'block';
+}
+
+function cancel () {
+  memoryStop = clock.innerText;
+  memoryTotalTime = totalTime.innerText;
+  timerResult();
+  clearInterval(clearTimer);
+  clearInterval(ticTac);
+  clearInterval(ticTac2);
+  clearInterval(reset);
+  clearInterval(reset2);
+  clearInterval(resetTotalCount);
+  progressBar.className = '';
+  remainingTime.innerText = '';
+  progressBar2.className = '';
+  remainingTime2.innerText = '';
+  totalTime.innerText = '';
+
+  timerCancelBtn.disabled = true;
+  timerCancelBtn.style.backgroundImage = '';
+  timerCancelBtn.className = 'DeactivationCancel';
+
+  timerStartBtn.disabled = false;
+  timerStartBtn.style.backgroundImage = '';
+  timerStartBtn.className = 'ActivationStart waitingClick';
+  timerStartBtn.onclick = timerFirstScreen;
+}
+
+function timerFirstScreen () {
+  for (let i = 0; i < resultInfo.length; i++) {
+    resultInfo[i].innerText = '';
+  } //  htmlCollection 순회
+  resultArea.style.display = 'none';
+  timerOption.style.display = 'block';
+  timerStartBtn.onclick = timerStart;
+  timer.style.visibility = 'visible';
+}
+
+function popUpTimer () {
+  if (window.innerWidth < 1128) {
+    deleteTaxiWindow();
+    deleteQuestWindow();
+  }
+
+  if (timer.style.visibility === 'visible') {
+    timer.style.visibility = 'hidden';
+  } else {
+    timer.style.visibility = 'visible';
+    tooltipText.classList.add('hidden');
+  }
+}
+
+tictacArea.style.display = 'none';
+resultArea.style.display = 'none';
+
+function timerResult () {
+  result_startTime.innerText = `시작: ${memoryStart}`;
+  result_stopTime.innerText = `종료: ${memoryStop}`;
+  result_totalCount.innerText = `${memoryTotalTime}`;
+  result_expCoupon.innerHTML += `x${memoryExpcoupon}`;
+  result_item.innerHTML += `x${memoryItem}`;
+}
+
+timerNpc.addEventListener('click', popUpTimer);
+timerStartBtn.addEventListener('mouseover', () => timerStartBtn.style.backgroundImage = 'url(\'' + BtOkMouseOver.src + '\')');
+timerStartBtn.addEventListener('mouseleave', () => timerStartBtn.style.backgroundImage = 'url(\'' + BtOkNormal.src + '\')');
+timerStartBtn.addEventListener('mousedown', () => timerStartBtn.style.backgroundImage = 'url(\'' + BtOkPressed.src + '\')');
+timerStartBtn.addEventListener('mouseup', () => timerStartBtn.style.backgroundImage = 'url(\'' + BtOkNormal.src + '\')');
+
+timerCancelBtn.addEventListener('mouseover', () => timerCancelBtn.style.backgroundImage = 'url(\'' + BtCancelMouseOver.src + '\')');
+timerCancelBtn.addEventListener('mouseleave', () => timerCancelBtn.style.backgroundImage = 'url(\'' + BtCancelNormal.src + '\')');
+timerCancelBtn.addEventListener('mousedown', () => timerCancelBtn.style.backgroundImage = 'url(\'' + BtCancelPressed.src + '\')');
+timerCancelBtn.addEventListener('mouseup', () => timerCancelBtn.style.backgroundImage = 'url(\'' + BtCancelNormal.src + '\')');
